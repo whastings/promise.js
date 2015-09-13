ccf2a0d644c65076e101237dddba5cbbcfa8b620
Will Hastings <will@objectdotcreate.net>
2015-09-13 10:35:02 -0700

    Set up build with Webpack.

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  entry: './Promise.js',
+  output: {
+    filename: 'dist/Promise.js',
+    library: 'Promise',
+    libraryTarget: 'umd',
+    sourcePrefix: '  '
+  },
+  module: {
+    loaders: [
+      {
+        loader: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      }
+    ]
+  },
+  devtool: 'hidden-source-map'
+};
